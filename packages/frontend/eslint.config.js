b98027e68dfb27900410ba87c03d8396a48fcde1
danielz0102 <danyzglez@hotmail.com>
2025-11-22 18:14:36 -0600

    chore(frontend): add jsx-a11y ESLint config

--- packages/frontend/eslint.config.js
+++ packages/frontend/eslint.config.js
@@ -1,12 +1,13 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook"
 
+import { defineConfig, globalIgnores } from "eslint/config"
 import js from "@eslint/js"
 import globals from "globals"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
+import jsxA11y from "eslint-plugin-jsx-a11y"
 import tseslint from "typescript-eslint"
-import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
   globalIgnores(["dist", "node_modules"]),
@@ -17,6 +18,7 @@ export default defineConfig([
       tseslint.configs.strictTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      jsxA11y.flatConfigs.strict,
       ...storybook.configs["flat/recommended"],
     ],
     languageOptions: {
