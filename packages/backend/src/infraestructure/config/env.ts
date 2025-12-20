1bafcba1c076c5a3396468a3fba8df864d5ec901
danielz0102 <danyzglez@hotmail.com>
2025-12-19 19:36:43 -0600

    refactor(config): enhance environment configuration and validation

--- packages/backend/src/infraestructure/config/env.ts
+++ packages/backend/src/infraestructure/config/env.ts
@@ -3,21 +3,48 @@ import z from "zod"
 
 dotenv.config({ quiet: true })
 
+const envSchema = z
+  .enum(["development", "production", "test"])
+  .default("development")
+
+const NODE_ENV = envSchema.parse(process.env.NODE_ENV)
+
 const configSchema = z.object({
   PORT: z.coerce.number().default(3000),
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
-  CLIENT_ORIGIN: z.url().default("*"),
+  CLIENT_ORIGIN: z.url(),
   DATABASE_URL: z.url(),
+  SALT: z.number().default(10),
+  JWT_SECRET: z.string().nonempty(),
 })
 
-export const { PORT, NODE_ENV, CLIENT_ORIGIN, DATABASE_URL } =
-  configSchema.parse(process.env)
+export const { PORT, CLIENT_ORIGIN, DATABASE_URL, SALT, JWT_SECRET } =
+  configSchema.parse({
+    PORT: process.env.PORT,
+    CLIENT_ORIGIN: getClientOrigin(),
+    DATABASE_URL: getDbUrl(),
+    SALT: getSalt(),
+    JWT_SECRET: process.env.JWT_SECRET,
+  })
 
-export const SALT = (() => {
+function getClientOrigin() {
+  if (NODE_ENV === "production") {
+    return process.env.CLIENT_ORIGIN
+  }
+
+  return "*"
+}
+
+function getDbUrl() {
+  if (NODE_ENV === "test") {
+    return process.env.TEST_DB_URL
+  }
+
+  return process.env.DATABASE_URL
+}
+
+function getSalt() {
   if (NODE_ENV === "test") return 1
   if (NODE_ENV === "production") return 12
 
   return 10
-})()
+}
