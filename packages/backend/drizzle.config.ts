268869cf18b45169607432f374a93b7bb92f11ef
danielz0102 <danyzglez@hotmail.com>
2025-12-20 23:54:32 -0600

    fix(backend): fix env variable in drizzle config

--- packages/backend/drizzle.config.ts
+++ packages/backend/drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "drizzle-kit"
+import { DATABASE_URL } from "./src/infraestructure/config/env"
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/infraestructure/db/drizzle/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    url: process.env.DATABASE_URL!,
+    url: DATABASE_URL,
   },
 })
