66dcb908cb1b1676cc8d52074f692d5e6a56a8a5
danielz0102 <danyzglez@hotmail.com>
2026-01-23 15:59:17 -0600

    refactor(api): add custom error for invalid image types

--- apps/api/src/presentation/middlewares/upload-picture.ts
+++ apps/api/src/presentation/middlewares/upload-picture.ts
@@ -1,13 +1,16 @@
 import { allowedImageMimeTypes } from "#domain/constants/allowed-image-mime-types.js"
+import { createCustomError } from "#lib/create-custom-error.js"
 import type { Request, Response, NextFunction } from "express"
 import multer from "multer"
+
+const InvalidImageTypeError = createCustomError("InvalidImageTypeError")
 
 const upload = multer({
   storage: multer.memoryStorage(),
   fileFilter(_, file, cb) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     if (!allowedImageMimeTypes.includes(file.mimetype as any)) {
-      return cb(new Error("INVALID_IMAGE_TYPE"))
+      return cb(new InvalidImageTypeError("Invalid image type"))
     }
 
     cb(null, true)
@@ -16,7 +19,7 @@ const upload = multer({
 
 export function uploadPicture(req: Request, res: Response, next: NextFunction) {
   upload(req, res, (err) => {
-    if (err instanceof Error && err.message === "INVALID_IMAGE_TYPE") {
+    if (err instanceof InvalidImageTypeError) {
       return res.status(400).json({
         error: "Invalid image type",
         allowedTypes: allowedImageMimeTypes,
