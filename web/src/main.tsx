edaaa228df448dc21b683f5a1cdb5ec941fadbee
danielz0102 <danyzglez@hotmail.com>
2026-02-01 22:44:13 -0600

    feat: add Inter font

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,17 @@
+// Supports weights 100-900
+import "@fontsource-variable/inter"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import { Router } from "./router.tsx"
 
-createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root")
+
+if (!root) {
+  throw new Error("Failed to find the root element")
+}
+
+createRoot(root).render(
   <StrictMode>
     <Router />
   </StrictMode>
