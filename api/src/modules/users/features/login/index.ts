72e43b6af9bcedb19ff6431aa054679f736411fc
danielz0102 <danyzglez@hotmail.com>
2026-02-13 16:26:55 -0600

    refactor(api): move login rate limit to pertinent file

--- api/src/modules/users/features/login/index.ts
+++ api/src/modules/users/features/login/index.ts
@@ -1,6 +1,7 @@
 import { BcryptHasher } from "#modules/users/implementations/bcrypt-hasher.js"
 import { DrizzleUserRepository } from "#modules/users/implementations/drizzle-user-repository.js"
 import { SALT } from "#shared/config/env.js"
+import { rateLimit } from "express-rate-limit"
 import { loginHandler } from "./login-handler.js"
 import { LoginService } from "./login-service.js"
 import { validateLogin } from "./validate-login.js"
@@ -10,4 +11,15 @@ const service = new LoginService({
   hasher: new BcryptHasher(SALT),
 })
 
-export const loginMiddleware = [validateLogin, loginHandler(service)]
+const loginRateLimit = rateLimit({
+  windowMs: 60 * 1000,
+  limit: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+
+export const loginMiddleware = [
+  validateLogin,
+  loginRateLimit,
+  loginHandler(service),
+]
--- api/src/modules/users/index.ts
+++ api/src/modules/users/index.ts
@@ -1,17 +1,10 @@
 import { Router } from "express"
-import rateLimit from "express-rate-limit"
 import { loginMiddleware } from "./features/login/index.js"
 import { registrationMiddleware } from "./features/register/index.js"
 
 const router = Router()
-const loginRateLimit = rateLimit({
-  windowMs: 60 * 1000,
-  limit: 10,
-  standardHeaders: true,
-  legacyHeaders: false,
-})
 
-router.get("/", loginRateLimit, loginMiddleware)
+router.get("/", loginMiddleware)
 router.post("/", registrationMiddleware)
 
 export { router as usersRouter }
