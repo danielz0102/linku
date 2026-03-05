8a315ce5b815bba1b792d9c3bc43ebc586748e76
danielz0102 <danyzglez@hotmail.com>
2026-03-04 19:06:04 -0600

    refactor(api): inline createUploadSignature function

--- apps/api/src/users/infrastructure/handlers/upload-signature-handler.ts
+++ apps/api/src/users/infrastructure/handlers/upload-signature-handler.ts
@@ -1,4 +1,12 @@
-import { createUploadSignature } from "#shared/providers/upload-image.js"
+import {
+  cloudinaryClient,
+  CloudinaryFolders,
+} from "#shared/config/cloudinary.js"
+import {
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_NAME,
+} from "#shared/config/env.js"
 import type { LinkuAPI } from "@linku/api-contract"
 import type { RequestHandler } from "express"
 
@@ -6,5 +14,20 @@ export const uploadSignatureHandler: RequestHandler<
   never,
   LinkuAPI.UploadSignature["ResponseBody"]
 > = (_req, res) => {
-  return res.status(200).json(createUploadSignature())
+  const timestamp = Math.round(new Date().getTime() / 1000)
+  const signature = cloudinaryClient.utils.api_sign_request(
+    {
+      folder: CloudinaryFolders.PROFILE_PICTURES,
+      timestamp,
+    },
+    CLOUDINARY_API_SECRET
+  )
+
+  return res.status(200).json({
+    signature,
+    timestamp,
+    cloudName: CLOUDINARY_NAME,
+    apiKey: CLOUDINARY_API_KEY,
+    folder: CloudinaryFolders.PROFILE_PICTURES,
+  })
 }
