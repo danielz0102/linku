74095eae16bd26e0519d8b32a27efc5ab545dd3b
danielz0102 <danyzglez@hotmail.com>
2026-03-04 19:01:39 -0600

    refactor(api): extract cloudinary configuration

--- apps/api/src/shared/providers/upload-image.ts
+++ apps/api/src/shared/providers/upload-image.ts
@@ -1,25 +1,19 @@
 import {
+  cloudinaryClient,
+  CloudinaryFolders,
+} from "#shared/config/cloudinary.js"
+import {
   CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET,
   CLOUDINARY_NAME,
 } from "#shared/config/env.js"
 import type { LinkuAPI } from "@linku/api-contract"
-import cloudinary from "cloudinary"
-
-cloudinary.v2.config({
-  cloud_name: CLOUDINARY_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
-  secure: true,
-})
-
-const folder = "linku/profile-pictures"
 
 export function createUploadSignature(): LinkuAPI.UploadSignature["ResponseBody"] {
-  const timestamp = Math.floor(Date.now() / 1000)
-  const signature = cloudinary.v2.utils.api_sign_request(
+  const timestamp = Math.round(new Date().getTime() / 1000)
+  const signature = cloudinaryClient.utils.api_sign_request(
     {
-      folder,
+      folder: CloudinaryFolders.PROFILE_PICTURES,
       timestamp,
     },
     CLOUDINARY_API_SECRET
@@ -30,6 +24,6 @@ export function createUploadSignature(): LinkuAPI.UploadSignature["ResponseBody"
     timestamp,
     cloudName: CLOUDINARY_NAME,
     apiKey: CLOUDINARY_API_KEY,
-    folder,
+    folder: CloudinaryFolders.PROFILE_PICTURES,
   }
 }
