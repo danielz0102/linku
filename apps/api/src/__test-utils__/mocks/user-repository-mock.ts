3934ea0b1fba5c651818db7a3429e5aca707b826
danielz0102 <danyzglez@hotmail.com>
2026-03-08 01:16:28 -0600

    fix(api): fix user repository mock

--- apps/api/src/__test-utils__/mocks/user-repository-mock.ts
+++ apps/api/src/__test-utils__/mocks/user-repository-mock.ts
@@ -3,5 +3,6 @@ import type { UserRepository } from "~/users/application/ports/user-repository.j
 export class UserRepositoryMock implements UserRepository {
   create = vi.fn<UserRepository["create"]>()
   findOne = vi.fn<UserRepository["findOne"]>()
+  search = vi.fn<UserRepository["search"]>()
   update = vi.fn<UserRepository["update"]>()
 }
