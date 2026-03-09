23e6ab4967f39725e035a01d48b6667eb28ffda4
danielz0102 <danyzglez@hotmail.com>
2026-03-09 16:31:20 -0600

    refactor(web): remove unnecessary check

--- apps/web/src/users/public-profile.tsx
+++ apps/web/src/users/public-profile.tsx
@@ -35,8 +35,6 @@ export default function PublicProfile() {
     )
   }
 
-  if (!user) return null
-
   return (
     <main className="flex size-full flex-col items-center justify-center gap-4 p-4 text-center">
       <img
