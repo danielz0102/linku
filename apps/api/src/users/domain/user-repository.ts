26271a6e6fca88d5219bfbbc3fdbe4c5ee74dea5
danielz0102 <danyzglez@hotmail.com>
2026-03-11 02:10:22 -0600

    refactor(api): change UserRepository declaration

--- apps/api/src/users/domain/user-repository.ts
+++ apps/api/src/users/domain/user-repository.ts
@@ -1,11 +1,11 @@
 import { User } from "#users/domain/user.js"
 
 export interface UserRepository {
-  create: (newUser: NewUser) => Promise<User>
-  findOne: (filters: UserFilters) => Promise<User | undefined>
-  search: (filters: UserFilters, options?: SearchOptions) => Promise<User[]>
-  update: (id: string, data: UpdateData) => Promise<User>
-  save: (user: User) => Promise<void>
+  create(newUser: NewUser): Promise<User>
+  findOne(filters: UserFilters): Promise<User | undefined>
+  search(filters: UserFilters, options?: SearchOptions): Promise<User[]>
+  update(id: string, data: UpdateData): Promise<User>
+  save(user: User): Promise<void>
 }
 
 export type NewUser = {
