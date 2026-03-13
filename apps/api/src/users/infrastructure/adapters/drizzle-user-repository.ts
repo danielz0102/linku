f7270affd3fcf66824da0468b6a5c7087ae46896
danielz0102 <danyzglez@hotmail.com>
2026-03-12 18:30:06 -0600

    refactor(api): streamline findExisting and checkUniqueness methods in DrizzleUserRepository

--- apps/api/src/users/infrastructure/adapters/drizzle-user-repository.ts
+++ apps/api/src/users/infrastructure/adapters/drizzle-user-repository.ts
@@ -5,7 +5,7 @@ import type {
   UserRepository,
 } from "#users/domain/user-repository.js"
 import { User } from "#users/domain/user.js"
-import { and, eq, not, or } from "drizzle-orm"
+import { and, eq, not, or, type SQL } from "drizzle-orm"
 
 export class DrizzleUserRepository implements UserRepository {
   async save(user: User): Promise<void> {
@@ -18,49 +18,45 @@ export class DrizzleUserRepository implements UserRepository {
     })
   }
 
-  async findExisting({
-    id,
-    username,
-    email,
-  }: Partial<UniqueFields>): Promise<User | undefined> {
-    if (!username && !email && !id) {
-      throw new Error("At least one filter must be provided")
-    }
+  async findExisting(fields: Partial<UniqueFields>): Promise<User | undefined> {
+    const conditions = this.uniqueFieldsToConditions(fields)
 
     return db
       .select()
       .from(usersTable)
-      .where(
-        and(
-          id ? eq(usersTable.id, id.value) : undefined,
-          username ? eq(usersTable.username, username) : undefined,
-          email ? eq(usersTable.email, email.value) : undefined
-        )
-      )
+      .where(and(...Object.values(conditions)))
       .then(([r]) => (r ? new User(r) : undefined))
   }
 
-  async checkUniqueness({
-    id,
-    username,
-    email,
-  }: Partial<UniqueFields>): Promise<User | undefined> {
-    if (!username && !email && !id) {
-      throw new Error("At least one filter must be provided")
-    }
+  async checkUniqueness(
+    fields: Partial<UniqueFields>
+  ): Promise<User | undefined> {
+    const { id, username, email } = this.uniqueFieldsToConditions(fields)
 
     return db
       .select()
       .from(usersTable)
-      .where(
-        and(
-          id ? not(eq(usersTable.id, id.value)) : undefined,
-          or(
-            username ? eq(usersTable.username, username) : undefined,
-            email ? eq(usersTable.email, email.value) : undefined
-          )
-        )
-      )
+      .where(and(id ? not(id) : undefined, or(username, email)))
       .then(([r]) => (r ? new User(r) : undefined))
+  }
+
+  private uniqueFieldsToConditions({
+    id,
+    username,
+    email,
+  }: Partial<UniqueFields>): Partial<{
+    id: SQL
+    username: SQL
+    email: SQL
+  }> {
+    if (!username && !email && !id) {
+      throw new Error("At least one filter must be provided")
+    }
+
+    return {
+      id: id ? eq(usersTable.id, id.value) : undefined,
+      username: username ? eq(usersTable.username, username) : undefined,
+      email: email ? eq(usersTable.email, email.value) : undefined,
+    }
   }
 }
