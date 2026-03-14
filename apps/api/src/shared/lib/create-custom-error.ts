74ffd61cba5f14c1241fa1266760dfe17fce0d8e
danielz0102 <danyzglez@hotmail.com>
2026-03-13 22:56:01 -0600

    refactor(api): remove unused createCustomError function

--- apps/api/src/shared/lib/create-custom-error.ts
+++
@@ -1,8 +0,0 @@
-export function createCustomError(name: string) {
-  return class extends Error {
-    constructor(message: string, options?: ErrorOptions) {
-      super(message, options)
-      this.name = name
-    }
-  }
-}
