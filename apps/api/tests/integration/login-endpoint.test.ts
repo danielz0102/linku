2a05d9e3d8befbd9d1083831613499a1235b9904
danielz0102 <danyzglez@hotmail.com>
2026-03-19 18:29:53 -0600

    test(api): wrap app building into context

--- apps/api/tests/integration/login-endpoint.test.ts
+++ apps/api/tests/integration/login-endpoint.test.ts
@@ -7,19 +7,18 @@ import { DrizzleUserRepository } from "~/shared/adapters/drizzle-user-repository
 import { createAuthContext } from "~tests/fixtures/auth-context.ts"
 import { createTestApp } from "~tests/helpers/app-builder.ts"
 
-const it = createAuthContext()
+const it = createAuthContext().extend("app", ({ dbClient }) => {
+  const app = createTestApp()
+  const useCase = new Login({
+    userRepo: new DrizzleUserRepository(dbClient),
+    hasher: new BcryptHasher(1),
+  })
+  app.post("/auth/login", new LoginEndpoint(useCase).build(false))
+  return app
+})
 
 describe("POST /auth/login", () => {
-  const app = createTestApp()
-
-  it.beforeAll(({ dbClient }) => {
-    const repo = new DrizzleUserRepository(dbClient)
-    const hasher = new BcryptHasher(1)
-    const useCase = new Login({ userRepo: repo, hasher })
-    app.post("/auth/login", new LoginEndpoint(useCase).build(false))
-  })
-
-  it("sends a 200 response with user data and session cookie", async ({ registeredUser }) => {
+  it("sends a 200 response with user data and session cookie", async ({ app, registeredUser }) => {
     const { credentials, publicData } = registeredUser
 
     await request(app)
@@ -30,7 +29,7 @@ describe("POST /auth/login", () => {
       .expect(publicData)
   })
 
-  it("sends a 401 response if credentials are invalid", async () => {
+  it("sends a 401 response if credentials are invalid", async ({ app }) => {
     await request(app)
       .post("/auth/login")
       .send({ username: "invalid", password: "invalid" })
