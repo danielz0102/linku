8daac6946e3475570c30ff641e39ec46f978e68d
danielz0102 <danyzglez@hotmail.com>
2026-03-19 23:44:40 -0600

    test(api): remove unused UserRepositoryMock test helper

--- apps/api/tests/helpers/users/user-repository-mock.ts
+++
@@ -1,7 +0,0 @@
-import type { UserRepository } from "~/core/users/user-repository.js"
-
-export class UserRepositoryMock implements UserRepository {
-  findOne = vi.fn<UserRepository["findOne"]>()
-  findConflict = vi.fn<UserRepository["findConflict"]>()
-  save = vi.fn<UserRepository["save"]>()
-}
