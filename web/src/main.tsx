5a95b35ebe70b3ea7f467ebfc8cf50e62d3c8a7d
danielz0102 <danyzglez@hotmail.com>
2026-04-03 18:01:31 -0600

    chore(web): install Tailwind and custom font

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
+// Supports weights 200-900
+import "@fontsource-variable/nunito-sans/wght.css"
 import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
