27c84804567e72a6c4642f5a32772fbfd3f2bc9b
danielz0102 <danyzglez@hotmail.com>
2026-04-11 16:58:29 -0600

    refactor(api): remove dead code

--- api/src/modules/users/commands/sign-up/sign-up-service.ts
+++ api/src/modules/users/commands/sign-up/sign-up-service.ts
@@ -4,9 +4,6 @@ import { eq } from "drizzle-orm"
 import { db } from "../../../../db/drizzle/drizzle-client.ts"
 import { users } from "../../../../db/drizzle/schemas.ts"
 import { SALT } from "../../../../env.ts"
-import type { UserRepository } from "../../../../shared/interfaces/user-repository.ts"
-import type { PublicUser } from "../../../../shared/user.ts"
-import type { PasswordHasher } from "../../interfaces/password-hasher.ts"
 
 type SignUpCommand = {
   username: string
@@ -39,35 +36,4 @@ export async function signUp(cmd: SignUpCommand): Promise<string | undefined> {
     .then((r) => r[0]!.id)
 
   return id
-}
-
-export class SignUpService {
-  constructor(
-    private readonly users: UserRepository,
-    private readonly hasher: PasswordHasher
-  ) {}
-
-  async execute(data: {
-    username: string
-    password: string
-    firstName: string
-    lastName: string
-  }): Promise<PublicUser | null> {
-    const existingUser = await this.users.findByUsername(data.username)
-
-    if (existingUser) return null
-
-    const hashedPassword = await this.hasher.hash(data.password)
-
-    const newUser = await this.users.create({
-      username: data.username,
-      hashedPassword,
-      firstName: data.firstName,
-      lastName: data.lastName,
-    })
-
-    const { hashedPassword: _, ...publicUser } = newUser
-
-    return publicUser
-  }
 }
--- api/src/modules/users/interfaces/password-hasher.ts
+++
@@ -1,6 +0,0 @@
-export abstract class PasswordHasher {
-  constructor(protected readonly salt: number) {}
-
-  abstract hash(password: string): Promise<string>
-  abstract compare(password: string, hashedPassword: string): Promise<boolean>
-}
