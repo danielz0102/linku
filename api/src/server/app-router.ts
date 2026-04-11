73293ba6ca54adb3f38366fd13e20b210248af74
danielz0102 <danyzglez@hotmail.com>
2026-04-11 16:22:11 -0600

    refactor(api): rename "auth" module to "users"

--- api/src/server/app-router.ts
+++ api/src/server/app-router.ts
@@ -1,4 +1,4 @@
-import { auth } from "../modules/auth/auth-router.ts"
+import { auth } from "../modules/users/auth-router.ts"
 import { publicProcedure, router } from "../shared/trpc.ts"
 
 export const appRouter = router({
