7800875ede9547fd8bb5455e64f0475af1bb23d9
danielz0102 <danyzglez@hotmail.com>
2026-04-11 23:18:39 -0600

    feat(api): add whoami procedure

--- api/src/server/routers/auth-router.ts
+++ api/src/server/routers/auth-router.ts
@@ -1,5 +1,10 @@
 import { loginProcedure } from "#modules/users/commands/login/login-procedure.ts"
 import { signUpProcedure } from "#modules/users/commands/sign-up/sign-up-procedure.ts"
+import { whoamiProcedure } from "#modules/users/queries/whoami/whoami-procedure.ts"
 import { router } from "#shared/trpc.ts"
 
-export const auth = router({ signUp: signUpProcedure, login: loginProcedure })
+export const auth = router({
+  signUp: signUpProcedure,
+  login: loginProcedure,
+  whoami: whoamiProcedure,
+})
