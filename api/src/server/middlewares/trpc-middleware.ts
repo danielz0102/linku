0bf9987f4b0d8544acc67ea29c1b0838caaa0bb8
danielz0102 <danyzglez@hotmail.com>
2026-04-13 01:02:17 -0600

    refactor(api): remove app router exports

--- api/src/server/middlewares/trpc-middleware.ts
+++ api/src/server/middlewares/trpc-middleware.ts
@@ -1,6 +1,6 @@
 import * as trpcExpress from "@trpc/server/adapters/express"
 
-import { appRouter } from "#server/app-router.ts"
+import { appRouter } from "#server/routers/app-router.ts"
 import { createContext } from "#shared/trpc.ts"
 
 export const trpcMiddleware = trpcExpress.createExpressMiddleware({
