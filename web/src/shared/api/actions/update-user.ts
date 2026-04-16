ca0dc0d7d9e0fd80571b1d9aedf1c16f6a7afa60
danielz0102 <danyzglez@hotmail.com>
2026-04-15 18:55:32 -0600

    feat(api): add updateUser action to API

---
+++ web/src/shared/api/actions/update-user.ts
@@ -0,0 +1,30 @@
+import { API_URL } from "~/env"
+
+type UpdateUserPayload = {
+  firstName: string
+  lastName: string
+  username: string
+  profilePictureUrl: string | null
+  bio: string | null
+}
+
+export async function updateUser(data: UpdateUserPayload): Promise<boolean> {
+  const res = await fetch(`${API_URL}/users/me`, {
+    credentials: "include",
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+
+  if (!res.ok) {
+    if (res.status === 409) {
+      return false
+    }
+
+    throw new Error("Failed to update user")
+  }
+
+  return true
+}
--- web/src/shared/api/api.ts
+++ web/src/shared/api/api.ts
@@ -1,8 +1,9 @@
 import { logOut } from "./actions/log-out"
 import { login } from "./actions/login"
 import { signUp } from "./actions/sign-up"
+import { updateUser } from "./actions/update-user"
 import { whoami } from "./actions/whoami"
 
 export const api = {
-  users: { whoami, login, signUp, logOut },
+  users: { whoami, login, signUp, logOut, updateUser },
 }
