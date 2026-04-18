9f3eace378cc7bef5096fa7d08c8dbd59f1ff523
danielz0102 <danyzglez@hotmail.com>
2026-04-18 17:45:00 -0600

    feat(web): add service to upload an image to Cloudinary

---
+++ web/src/modules/users/pages/profile/upload-image.ts
@@ -0,0 +1,35 @@
+import { api } from "~/shared/api/api"
+
+export async function uploadImage(file: File): Promise<{ url: string }> {
+  const signature = await api.users.getCloudinarySignature()
+  const formData = new FormData()
+
+  formData.append("file", file)
+  formData.append("api_key", signature.api_key)
+  formData.append("timestamp", signature.timestamp.toString())
+  formData.append("signature", signature.signature)
+  formData.append("folder", signature.folder)
+  formData.append("public_id", signature.public_id)
+
+  const cloudinaryRes = await fetch(
+    `https://api.cloudinary.com/v1_1/${signature.cloudName}/image/upload`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  )
+
+  if (!cloudinaryRes.ok) {
+    throw new Error("Failed to upload image", {
+      cause: { status: cloudinaryRes.status },
+    })
+  }
+
+  const data = (await cloudinaryRes.json()) as { secure_url?: string }
+
+  if (!data.secure_url) {
+    throw new Error("Uploaded image URL is missing", { cause: data })
+  }
+
+  return { url: data.secure_url }
+}
