261df696cfb493c4470ce25b3ec0a5a1c99fa925
danielz0102 <danyzglez@hotmail.com>
2026-04-20 01:50:03 -0600

    refactor(web): remove getters

--- web/src/modules/chats/domain/chat.ts
+++ web/src/modules/chats/domain/chat.ts
@@ -5,14 +5,14 @@ import type { Message } from "./message"
 export class Chat {
   readonly id: string
   #sender: ChatMember
-  #lastMessage: Message
-  #time: ChatDate
+  readonly lastMessage: Message
+  readonly time: ChatDate
 
   private constructor(id: string, sender: ChatMember, lastMessage: Message) {
     this.id = id
     this.#sender = sender
-    this.#lastMessage = lastMessage
-    this.#time = new ChatDate(lastMessage.createdAt)
+    this.lastMessage = lastMessage
+    this.time = new ChatDate(lastMessage.createdAt)
   }
 
   static create(data: { id: string; sender: ChatMember; lastMessage: Message }) {
@@ -29,13 +29,5 @@ export class Chat {
 
   get name(): string {
     return this.#sender.name
-  }
-
-  get lastMessage(): Message {
-    return this.#lastMessage
-  }
-
-  get time(): ChatDate {
-    return this.#time
   }
 }
