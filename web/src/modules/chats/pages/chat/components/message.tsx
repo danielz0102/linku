eb337a227afc3066b476a3b1b34f15308599ede8
danielz0102 <danyzglez@hotmail.com>
2026-04-21 17:30:49 -0600

    fix(web): reduce message max width

--- web/src/modules/chats/pages/chat/components/message.tsx
+++ web/src/modules/chats/pages/chat/components/message.tsx
@@ -11,7 +11,7 @@ export function Message({ message, peerId }: MessageProps) {
 
   return (
     <article
-      className={`flex max-w-[85%] flex-col gap-2 rounded-2xl p-3 wrap-break-word ${
+      className={`flex max-w-[50%] flex-col gap-2 rounded-2xl p-3 wrap-break-word ${
         isPeerMessage
           ? "self-start rounded-bl-sm bg-blue-100"
           : "self-end rounded-br-sm bg-blue-300 text-black"
