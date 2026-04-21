19037c3e3c3a0a58b0d4687c893ccfcc3aa3e465
danielz0102 <danyzglez@hotmail.com>
2026-04-21 15:37:13 -0600

    fix(web): update border color in message form component

--- web/src/modules/chats/pages/chat/message-form.tsx
+++ web/src/modules/chats/pages/chat/message-form.tsx
@@ -24,7 +24,7 @@ export function MessageForm({ onSubmit }: MessageFormProps) {
         onSubmit({ file, message: trimmedContent })
         e.currentTarget.reset()
       }}
-      className="text-foreground flex items-center gap-2 rounded-3xl border border-blue-300 bg-blue-100 px-4 py-2"
+      className="text-foreground flex items-center gap-2 rounded-3xl border border-blue-200 bg-blue-100 px-4 py-2"
     >
       <button
         type="button"
