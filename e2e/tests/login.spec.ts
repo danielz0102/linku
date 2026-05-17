27d148a0c7184a435524dba076d2daeaf964bc86
danielz0102 <danyzglez@hotmail.com>
2026-05-17 00:40:09 -0600

    tests(e2e): rename tests and wrap them into `describe`

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -2,30 +2,32 @@ import { expect } from "@playwright/test"
 
 import { test } from "../fixtures/registered-user.ts"
 
-test("logs in successfully with valid credentials", async ({ page, registeredUser }) => {
-  const { username, password } = registeredUser
+test.describe("Login Page", () => {
+  test("logs in successfully with valid credentials", async ({ page, registeredUser }) => {
+    const { username, password } = registeredUser
 
-  await page.goto("/")
-  const usernameInput = page.getByLabel("Username")
-  const passwordInput = page.getByLabel("Password")
-  const loginButton = page.getByRole("button", { name: "Login" })
+    await page.goto("/")
+    const usernameInput = page.getByLabel("Username")
+    const passwordInput = page.getByLabel("Password")
+    const loginButton = page.getByRole("button", { name: "Login" })
 
-  await usernameInput.fill(username)
-  await passwordInput.fill(password)
-  await loginButton.click()
+    await usernameInput.fill(username)
+    await passwordInput.fill(password)
+    await loginButton.click()
 
-  await expect(page.getByText("My Chats")).toBeVisible()
-})
+    await expect(page.getByText("My Chats")).toBeVisible()
+  })
 
-test("fails to login with invalid credentials", async ({ page }) => {
-  await page.goto("/")
-  const usernameInput = page.getByLabel("Username")
-  const passwordInput = page.getByLabel("Password")
-  const loginButton = page.getByRole("button", { name: "Login" })
+  test("rejects login with invalid credentials", async ({ page }) => {
+    await page.goto("/")
+    const usernameInput = page.getByLabel("Username")
+    const passwordInput = page.getByLabel("Password")
+    const loginButton = page.getByRole("button", { name: "Login" })
 
-  await usernameInput.fill("invalid_user")
-  await passwordInput.fill("invalid_password")
-  await loginButton.click()
+    await usernameInput.fill("invalid_user")
+    await passwordInput.fill("invalid_password")
+    await loginButton.click()
 
-  await expect(page.getByText("Invalid credentials")).toBeVisible()
+    await expect(page.getByText("Invalid credentials")).toBeVisible()
+  })
 })
