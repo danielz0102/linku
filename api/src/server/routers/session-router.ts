3816dd799afe7cbd376b0e5e60c024ad68523b50
danielz0102 <danyzglez@hotmail.com>
2026-05-19 16:31:35 -0600

    refactor(api): move login rate limit middleware to login module

--- api/src/server/routers/session-router.ts
+++ api/src/server/routers/session-router.ts
@@ -1,8 +1,8 @@
 import { Router } from "express"
 
 import { loginController } from "#modules/users/commands/login/login-http-controller.ts"
+import { loginRateLimitMiddleware } from "#modules/users/commands/login/login-rate-limit-middleware.ts"
 import { logoutController } from "#modules/users/commands/logout.ts/logout-http-controller.ts"
-import { loginRateLimitMiddleware } from "#server/middlewares/login-rate-limit-middleware.ts"
 
 export const sessionRouter = Router()
 
